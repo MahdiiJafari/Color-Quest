42e9902ad06dc9cd378108bad0c706a2f3f6df8c
mahdi <mahdi.jafarii.804@gmail.com>
2025-09-14 12:54:28 +0330

    adding color wheel

---
+++ script/script.js
@@ -0,0 +1,28 @@
+var colorPicker = new iro.ColorPicker("#color-picker", {
+  width: 240,
+  borderWidth: 10,
+  layout: [
+    {
+      component: iro.ui.Wheel,
+      options: {},
+    },
+  ],
+});
+
+const targetColorDiv = document.getElementById("target-color");
+const scoreSpan = document.getElementById("score");
+const roundSpan = document.getElementById("round");
+
+let round = 1;
+let totalScore = 0.0;
+let targetColor = generateRandomColor();
+
+targetColorDiv.style.background = targetColor.hex;
+
+function generateRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return { r, g, b, hex: `rgb(${r}, ${g}, ${b})` };
+}
+
